f943e19fb8f8f9277d68ba9604c3aa939b482a76
swisch <swisch.nick@gmail.com>
2026-09-12 23:32:48 +0400

    Update 1

---
+++ lekciebi/damatebiti_info/__init__dell__new__.py
@@ -0,0 +1,31 @@
+#__new__. Магический метод __Пример паттерна Singleton | Объектно-ориентированное программирование
+
+# __init__ მაგიკური მეთოდი ინით - შექმნა
+#__dell__ მაგიკური მეთოდი დელ - წაშლა - დესტრუქტორი
+
+# --------------
+# class __new__:
+#     def __new__(cls, *args, **kwargs):
+#         instance = super().__new__(cls)
+#         return instance
+
+#---------
+# class Lekciebi:
+#     def __init__(self, name, teacher, duration):
+#         self.name = name
+#         self.teacher = teacher
+#         self.duration = duration
+
+#     def __del__(self):
+#         print(f"Lesson '{self.name}' has been deleted.")
+
+
+# name = input("გთხოვთ შეიყვანოთ გაკვეთილის სახელი: ")
+# teacher = input("გთხოვთ შეიყვანოთ პრეპოდავტელის სახელი: ")
+# duration = int(input("გთხოვთ შეიყვანოთ გაკვეთილის ხანგრძლივობა წუთებში: "))
+
+# lesson1 = Lekciebi(name, teacher, duration)
+
+# print("გაკვეთილის სახელი:", lesson1.name)
+# print("პრეპოდავტელი:", lesson1.teacher)
+# print("ხანგრძლივობა:", lesson1.duration, "წუთი")
